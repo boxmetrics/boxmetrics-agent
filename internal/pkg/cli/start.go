b8c16a8b2b2869ae46a1b4becec881d3b2ff1d81
Laurent-PANEK <laurent.panek@ynov.com>
2019-06-22 17:08:59 +0200

    :ambulance: throw error when starting multiple server

--- internal/pkg/cli/start.go
+++ internal/pkg/cli/start.go
@@ -1,18 +1,28 @@
 package cli
 
 import (
+	"github.com/boxmetrics/boxmetrics-agent/internal/pkg/errors"
 	"os/exec"
 )
 
 // StartServer start agent server
 func StartServer() (int, error) {
-	server := exec.Command("bash", "-c", "./bin/server")
-	err := server.Start()
+	pid, err := readPID()
 	if err != nil {
 		return 0, err
 	}
 
-	pid, err := writePID(server.Process.Pid)
+	if pid != -1 {
+		return 0, errors.New("server already running")
+	}
+
+	server := exec.Command("bash", "-c", "./bin/server")
+	err = server.Start()
+	if err != nil {
+		return 0, err
+	}
+
+	pid, err = writePID(server.Process.Pid)
 	if err != nil {
 		return 0, err
 	}
